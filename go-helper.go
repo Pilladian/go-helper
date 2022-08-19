b48c71d2bab0250038c6d2914e07b2a52b7734e2
Pilladian <ph-zimm-acc@pm.me>
2022-08-19 08:14:24 +0200

    Exist() added

---
+++ go-helper.go
@@ -0,0 +1,15 @@
+package helper
+
+import "os"
+
+// Takes input string "path" and returns wether the file/directory exists on the hosts system.
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}

6506f9dcd583187367cf7fcbd8dc6556e91e657c
Pilladian <ph-zimm-acc@pm.me>
2022-08-19 08:22:29 +0200

    CreatePath() added

--- go-helper.go
+++ go-helper.go
@@ -1,8 +1,10 @@
 package helper
 
 import (
+	"errors"
 	"math/rand"
 	"os"
+	"strings"
 )
 
 // Takes input string "path" and returns wether the file/directory exists on the hosts system.
@@ -15,6 +17,25 @@ func Exists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+// Takes input string "path" and recursively creates the directories on the host system. If an error occurs, it will be returned.
+func CreatePath(path string) error {
+	existent, err := Exists(path)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	if !existent {
+		split_path := strings.Split(path, "/")
+		CreatePath(strings.Join(split_path[0:len(split_path)-1], "/"))
+
+		err := os.Mkdir(path, 0700)
+		if err != nil {
+			return errors.New(err.Error())
+		}
+	}
+	return nil
 }
 
 // Takes int "n" as input and return a random string of length "n" which contains random letters and digits

24d21b728c47b322fc9a44eda47e7f24690d8fe2
Pilladian <ph-zimm-acc@pm.me>
2022-08-19 13:24:27 +0200

    json comment fixed

--- validation.go
+++ validation.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Takes input string "data" and checks for basic JSON syntax. This check only supports simple JSON without arrays.
-// Example: {0: "success", "1": "success"} would match, while {0: ["fail", "fail"]} would fail.
+// Example: {"0": "success", "1": "success"} would match, while {"0": ["fail", "fail"]} would fail.
 func ValidateSimpleJSON(data string) error {
 	re, _ := regexp.Compile(` \{("(\d|\w)" *: *(\d+|"\w+") *, *)*"(\d|\w)" *: *(\d+|"\w+")\} `)
 	if !re.Match([]byte(fmt.Sprintf(" %s ", data))) {

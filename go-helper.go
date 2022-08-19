e5aefb5763dbf8232618da2889c2f854b8656d3e
Pilladian <ph-zimm-acc@pm.me>
2022-08-19 08:17:46 +0200

    RandomString() added

--- go-helper.go
+++ go-helper.go
@@ -1,6 +1,9 @@
 package helper
 
-import "os"
+import (
+	"math/rand"
+	"os"
+)
 
 // Takes input string "path" and returns wether the file/directory exists on the hosts system.
 func Exists(path string) (bool, error) {
@@ -12,4 +15,15 @@ func Exists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+// Takes int "n" as input and return a random string of length "n" which contains random letters and digits
+func RandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
 }

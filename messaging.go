b175dabbd54cbd780b9a8d0e25cc0568637b4d28
Pilladian <ph-zimm-acc@pm.me>
2022-08-29 17:05:43 +0200

    url escape

--- messaging.go
+++ messaging.go
@@ -3,11 +3,12 @@ package helper
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 // Takes input strings bot_token, chatID and message and sends a Telegram message to a given chat as specific bot
 func SendTelegramMessage(bot_token string, chatID string, message string) (int, error) {
-	send_text := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&parse_mode=Markdown&text=%s", bot_token, chatID, message)
+	send_text := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&parse_mode=Markdown&text=%s", bot_token, chatID, url.QueryEscape(message))
 	re, re_err := http.Get(send_text)
 	return re.StatusCode, re_err
 }

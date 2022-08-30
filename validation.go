717d86824ac67d067a7aa583a4a3d4b0a31a8fc8
Pilladian <ph-zimm-acc@pm.me>
2022-08-30 17:29:10 +0200

    added slash to simple JSON

--- validation.go
+++ validation.go
@@ -9,7 +9,7 @@ import (
 // Takes input string "data" and checks for basic JSON syntax. This check only supports simple JSON without arrays.
 // Example: {"0": "success", "1": "success"} would match, while {"0": ["fail", "fail"]} would fail.
 func ValidateSimpleJSON(data string) error {
-	re, _ := regexp.Compile(` \{("(\w|\+|:|_|-|\.)+" *: *(\d+|"(\w|\+|:|_|-|\.)+") *, *)*"(\w|\+|:|_|-|\.)+" *: *(\d+|"(\w|\+|:|_|-|\.)+")\} `)
+	re, _ := regexp.Compile(` \{("(\w|\+|:|_|-|\.)+" *: *(\d+|"(\w|\+|:|_|-|\.|\/)+") *, *)*"(\w|\+|:|_|-|\.)+" *: *(\d+|"(\w|\+|:|_|-|\.|\/)+")\} `)
 	if !re.Match([]byte(fmt.Sprintf(" %s ", data))) {
 		return errors.New("data did not match json format or contained invalid characters")
 	}
